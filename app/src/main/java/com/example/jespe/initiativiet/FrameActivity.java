4d8bcbd23a416a3b3db5e687c2eb1221764d7b24
MrKimberwood <jesper.xenia@gmail.com>
2018-01-11 21:04:56 +0100

    Working commit

--- app/src/main/java/com/example/jespe/initiativiet/FrameActivity.java
+++ app/src/main/java/com/example/jespe/initiativiet/FrameActivity.java
@@ -10,9 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
-public class FrameActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class FrameActivity extends AppCompatActivity {
 
     //FrameLayout fl;
 
@@ -26,51 +27,18 @@ public class FrameActivity extends AppCompatActivity implements NavigationView.O
                 .replace(R.id.frame, new TabFragment())
                 .commit();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+        navigationView.setNavigationItemSelectedListener(this);*/
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_profile) {
-            // Handle the camera action
-        } else if (id == R.id.nav_posts) {
-
-        } else if (id == R.id.nav_debates) {
-
-        } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_logout) {
-            //System.out.println("id: "+auth.getCurrentUser());
-            //auth.signOut();
-            //System.out.println(auth.getCurrentUser());
-            /*try {
-                if(auth.getCurrentUser() == null) {
-
-                    getSupportFragmentManager().beginTransaction()
-                            .add(R.id.fragmentContainer, new LoginFragment())
-                            .commit();
-                    finish();
-                }
-            } catch (Exception e) {e.printStackTrace();}
-        }
-        */
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        return true;
     }
 }
