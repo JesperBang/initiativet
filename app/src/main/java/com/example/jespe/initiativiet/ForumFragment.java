fd36b32756bad0732c5eb4fa627a90f4e8575dd2
Gustav Petersen <Rangewaver@gmail.com>
2018-01-17 19:17:10 +0100

    Forum types now work

--- app/src/main/java/com/example/jespe/initiativiet/ForumFragment.java
+++ app/src/main/java/com/example/jespe/initiativiet/ForumFragment.java
@@ -1,12 +1,10 @@
 package com.example.jespe.initiativiet;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -50,91 +48,94 @@ public class ForumFragment extends Fragment implements View.OnClickListener, Ada
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = i.inflate(R.layout.fragment_forum, container, false);
+
         return v;
     }
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    fab = (FloatingActionButton) view.findViewById(R.id.fab);
-        fab.setOnClickListener(this);
-    debat = (ListView) view.findViewById(R.id.debat);
 
-    lfs = new ArrayList<>();
+            fab = (FloatingActionButton) view.findViewById(R.id.fab);
+            fab.setOnClickListener(this);
+            debat = (ListView) view.findViewById(R.id.debat);
 
+            lfs = new ArrayList<>();
 
-    //Firebase Reference
-    fb =  FirebaseDatabase.getInstance().getReference().child("forumentries");
-    vEL = fb.addValueEventListener(new ValueEventListener() {
+            //Firebase Reference
+            fb = FirebaseDatabase.getInstance().getReference().child("forumentries");
+            vEL = fb.addValueEventListener(new ValueEventListener() {
 
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            lfs.clear();
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    lfs.clear();
 
-            for ( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
-                map = (Map) snapshot.getValue();
-                lfs.add(new Lovforslag(
-                    map.get("title"),
-                    map.get("content"),
-                    map.get("tagName"),
-                    map.get("type"),
-                    snapshot.getKey()
-                ));
+                        map = (Map) snapshot.getValue();
+                        lfs.add(new Lovforslag(
+                                map.get("title"),
+                                map.get("content"),
+                                map.get("tagName"),
+                                map.get("type"),
+                                snapshot.getKey()
+                        ));
 
-            }
-            debat.setAdapter(aa);
-        }
+                    }
+                    debat.setAdapter(aa);
 
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-    });
-
-    aa = new ArrayAdapter(getActivity(), R.layout.postlist, R.id.headline, lfs) {
-
-        @Override
-        public View getView(int position, View cachedView, ViewGroup parent) {
-
-            View v = super.getView(position, cachedView, parent);
-            if (position != lfs.size()) {
-                TextView headline = (TextView) v.findViewById(R.id.headline);
-                TextView category = (TextView) v.findViewById(R.id.category);
-                TextView description = (TextView) v.findViewById(R.id.description);
-                //TextView author = (TextView) v.findViewById(R.id.author);
-
-                headline.setText(lfs.get(position).getTitle());
-                category.setText(lfs.get(position).getTagName());
-                description.setText(lfs.get(position).getContent());
-                // author.setText(lfs.get(position).getAuthor());
-
-                switch (lfs.get(position).getTagName()) {
-                    case "Miljø":
-                        //parent.getChildAt(position).setBackgroundColor(Color.GREEN);
-                        break;
-                    case "Finans":
-                        //parent.getChildAt(position).setBackgroundColor(Color.BLUE);
-                        break;
-                    case "Integration":
-                        //parent.getChildAt(position).setBackgroundColor(Color.RED);
-                        break;
-                    case "Uddannelse":
-                        //parent.getChildAt(position).setBackgroundColor(Color.YELLOW);
-                        break;
-                    default:
-                        //parent.getChildAt(position).setBackgroundColor(Color.GRAY);
-                        break;
                 }
 
-                //return v;
-            }
-            return v;
-        }
-    };
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-    //debat = new ListView(this);
+                }
+            });
+
+            aa = new ArrayAdapter(getActivity(), R.layout.postlist, R.id.headline, lfs) {
+
+                @Override
+                public View getView(int position, View cachedView, ViewGroup parent) {
+
+                    View v = super.getView(position, cachedView, parent);
+                    if (position != lfs.size()) {
+                        TextView headline = (TextView) v.findViewById(R.id.headline);
+                        TextView category = (TextView) v.findViewById(R.id.category);
+                        TextView description = (TextView) v.findViewById(R.id.description);
+                        //TextView author = (TextView) v.findViewById(R.id.author);
+
+                        headline.setText(lfs.get(position).getTitle());
+                        category.setText(lfs.get(position).getTagName());
+                        description.setText(lfs.get(position).getContent());
+                        // author.setText(lfs.get(position).getAuthor());
+
+                        switch (lfs.get(position).getTagName()) {
+                            case "Miljø":
+                                //parent.getChildAt(position).setBackgroundColor(Color.GREEN);
+                                break;
+                            case "Finans":
+                                //parent.getChildAt(position).setBackgroundColor(Color.BLUE);
+                                break;
+                            case "Integration":
+                                //parent.getChildAt(position).setBackgroundColor(Color.RED);
+                                break;
+                            case "Uddannelse":
+                                //parent.getChildAt(position).setBackgroundColor(Color.YELLOW);
+                                break;
+                            default:
+                                //parent.getChildAt(position).setBackgroundColor(Color.GRAY);
+                                break;
+                        }
+
+                        //return v;
+                    }
+
+                    return v;
+                }
+            };
+
+            //debat = new ListView(this);
             debat.setOnItemClickListener(this);
-    //debat.setAdapter(aa);
-}
+            //debat.setAdapter(aa);
+        }
 
     @Override
     public void onClick(View v) {
@@ -145,20 +146,25 @@ public class ForumFragment extends Fragment implements View.OnClickListener, Ada
         }
 
     }
-    public void onItemClick(AdapterView<?> I, View v, int position, long id){
+
+    public void onItemClick(AdapterView<?> I, View v, int position, long id) {
         //Creating fragment and bundle for carrying data
         Fragment frag = new ForumItemFragment();
         Bundle bundle = new Bundle();
         bundle.putString("Key", lfs.get(position).getKey());
         frag.setArguments(bundle);
-        getFragmentManager().beginTransaction()
-            .replace(R.id.frame, frag)
-            .commit();
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, frag);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
     }
 
-    public void createPost(){
+    public void createPost() {
         getFragmentManager().beginTransaction()
-            .replace(R.id.frame, new CreatePostFragment())
-            .commit();
+                .replace(R.id.frame, new CreatePostFragment())
+                .addToBackStack(null)
+                .commit();
     }
 }
